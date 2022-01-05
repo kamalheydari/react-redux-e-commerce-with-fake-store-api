e2f91971124ca3d7e220410f48d620932d15ded9
kamal heydari <kamal3heydari@gmail.com>
2022-01-05 20:39:20 +0330

    Updates

---
+++ static/js/7.6b16d416.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-redux-fake-store"]=this["webpackJsonpreact-redux-fake-store"]||[]).push([[7],{50:function(e,t,s){"use strict";s.r(t);s(2);var c=s(17),r=s(1);t.default=function(){return Object(r.jsxs)("main",{children:[Object(r.jsx)(c.m,{}),Object(r.jsx)(c.s,{}),Object(r.jsx)(c.q,{}),Object(r.jsx)(c.g,{})]})}}}]);
+//# sourceMappingURL=7.6b16d416.chunk.js.map
